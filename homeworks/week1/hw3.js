d504e1d3faff59337646f0420c40f6289f97d574
Shin Lee <hi101072645@gmail.com>
2018-09-03 23:57:58 +0800

    week1 homework

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,8 @@
 function reverse(str) {
-
+    var revStr=''
+    var splStr=str.split('')
+    for(i=0;i<str.length;i++){
+      revStr+=splStr[str.length-i-1]
+    }
+    return(revStr)
 }
--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,7 +1,15 @@
 function join(str, concatStr) {
-
+    var joinStr=''
+    for(i=0;i<str.length;i++){
+        joinStr+=str[i]+concatStr
+    }
+    return(joinStr)
 }
 
 function repeat(str, times) {
-
+    var repStr=''
+    for(i=0;i<times;i++){
+        repStr += str
+    }
+    return(repStr)
 }
