ffc7c3898b1dffa4c570e957000c96a709931aa5
Shin Lee <hi101072645@gmail.com>
2018-09-12 00:12:09 +0800

    hw2

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,10 @@
 function stars(n) {
-
+    var stars = []
+    var star = '*'
+    for(var i=0;i<n;i++){
+        stars[i] = star
+        star = star + '*'
+    }
+    return stars
 }
-
 module.exports = stars;
