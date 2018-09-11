27ac148a9c783edcff1e3e0c59fd784c09a02107
Shin Lee <hi101072645@gmail.com>
2018-09-11 23:18:28 +0800

    hw2

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,9 @@
 function isPalindromes(str) {
-  
+    var strRev=''
+    for(i=0;i<str.length;i++){
+      strRev = strRev + str[str.length-i-1]
+    }
+    return str == strRev ? true :false
 }
 
 module.exports = isPalindromes
