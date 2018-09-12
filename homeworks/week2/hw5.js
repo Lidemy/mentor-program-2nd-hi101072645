2e2ab5d0ab99628c6ad6d1f84d3bb0c14b7288f1
hi101072645 in unotour <hi101072645@gmail.com>
2018-09-12 09:27:57 +0800

    fixed hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -4,8 +4,7 @@ function add(a, b) {
   var bArr = b.split("").reverse()
   var totalArr =[]
   a.length>=b.length ?  num = a.length : num = b.length
-  
-  for(var i=0;i<num;i++){
+  for(i=0;i<=num;i++){
     if(!aArr[i]){
       aArr[i] = 0
     }
@@ -13,13 +12,19 @@ function add(a, b) {
       bArr[i] = 0
     }
     totalArr[i]=parseInt(aArr[i]) + parseInt(bArr[i])
+  }
+  for(i=0;i<num;i++){
     if(totalArr[i]>9){
-      aArr[i+1] = aArr[i+1] + Math.floor(totalArr[i]/10)
+      totalArr[i+1] = totalArr[i+1] + Math.floor(totalArr[i]/10)
       totalArr[i] = totalArr[i] % 10
     }
   }
+  if(totalArr[num] == 0){
+    totalArr.pop()
+  }
    return totalArr.reverse().join('')
 }
+
 
 
 module.exports = add;
