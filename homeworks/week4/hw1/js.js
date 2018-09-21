e0046ac0e36f8dc0efa0454d9059b153a7cef3ce
hi101072645 in unotour <hi101072645@gmail.com>
2018-09-21 17:30:43 +0800

    hw1 html & css

---
+++ homeworks/week4/hw1/js.js
@@ -0,0 +1,15 @@
+
+
+function add(){
+
+}
+
+var screen = document.querySelector('.screen');
+var calculator = document.querySelector('.calculator');
+calculator.addEventListener('click',function(e){
+
+})
+
+function pressNumber(){
+
+}
